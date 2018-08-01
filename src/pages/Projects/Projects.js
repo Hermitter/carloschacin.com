cf04eb4c8bb2373cad7f876b97d80213c0780eb2
Carlos Chacin <chacin.carlos9798@gmail.com>
2018-07-31 22:53:44 -0400

    Fixed scrollbar shift & mobile view

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -14,7 +14,7 @@ export class Projects extends React.Component {
 
   render() {
     return(
-      <div id='Projects-container' className='animated fadeIn'>
+      <div id='projects-container' className='animated fadeIn'>
         <p style={{fontSize:'40px'}}>Current Page: Project Page</p>
         <p style={{fontSize:'40px'}}>Current Page: Project Page</p>
         <p style={{fontSize:'40px'}}>Current Page: Project Page</p>
