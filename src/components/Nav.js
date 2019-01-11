3d8e11aa7a918a21e35d9944d0ccd1feb8fe9441
Hermitter <chacin.carlos9798@gmail.com>
2019-01-10 23:09:01 -0500

    Added nav bar

---
+++ src/components/Nav.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Nav.css';
+import { Link } from "gatsby"
+import github_logo from '../../content/assets/github-logo.svg';
+
+export default ()=> {
+    return(
+        // Navigation Menu
+        <ul id='nav' className='animated zoomIn'>
+        {/* Home */}
+        <li id='nav-item'>
+            <Link id="nav-link" to="/">Home</Link>
+        </li>
+        {/* Github */}
+        <li id='nav-item'>
+            <a id="github-link" href='https://github.com/hermitter' target='_blank'>
+                <img id="github-logo" src={github_logo} />
+            </a>
+        </li>
+        {/* Blog */}
+            <li id='nav-item'>
+                <Link id="nav-link" to="/blog">Blog</Link>
+            </li>
+        </ul>
+    );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,15 @@
 import React from "react"
+import Nav from '../components/Nav'
 import Layout from '../components/Layout'
 
 export default () => (
-  <div>Hello world!
+  <div>
+    
+    <Nav/>
+    
+    Hello world!
     <div>Hello world!</div>
+
+
   </div>
 )
